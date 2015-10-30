84049bf7a6855e3b9321a5353bc6e9d04bf5906d
Jake <571014791@qq.com>
2015-10-30 13:46:29 +0800

    cccxss

--- app/src/main/java/com/example/qingtian/testproduct/MainActivity.java
+++ app/src/main/java/com/example/qingtian/testproduct/MainActivity.java
@@ -8,9 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        
+
                         .setAction("Action", null).show();
             }
         });
